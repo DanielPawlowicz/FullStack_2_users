ed796f2c5ac3474dc2740690f716d44a1369a0a5
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-03-12 18:07:49 +0100

    Posting data into database

---
+++ fullstack2-backend/src/main/java/com/Daniel/fullstack2backend/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.Daniel.fullstack2backend.controller;
+
+import com.Daniel.fullstack2backend.model.User;
+import com.Daniel.fullstack2backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+}
