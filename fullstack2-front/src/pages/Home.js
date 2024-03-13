6cee6e5853cfd086d112d874d0c03fb23abcdf5f
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-03-13 19:17:05 +0100

    Deleting users

--- fullstack2-front/src/pages/Home.js
+++ fullstack2-front/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Home() {
 
     const [users, setUsers] = useState([]);
+
+    const {id} = useParams();
 
     useEffect( () => {
         // console.log("Hello");
@@ -15,6 +17,11 @@ export default function Home() {
         const result = await axios.get("http://localhost:8080/users");
         // console.log(result.data);
         setUsers(result.data);
+    }
+
+    const deleteUser = async(id)=>{
+        await axios.delete(`http://localhost:8080/user/${id}`);
+        loadUsers();
     }
 
   return (
@@ -43,7 +50,9 @@ export default function Home() {
                                     <Link className='btn btn-outline-primary mx-2'
                                         to={`/edituser/${user.id}`}
                                     >Edit</Link>
-                                    <button className='btn btn-danger mx-2'>Delete</button>
+                                    <button className='btn btn-danger mx-2'
+                                        onClick={() => deleteUser(user.id)}
+                                    >Delete</button>
                                 </td>
                             </tr>
                         ))
