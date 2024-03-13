0b13b6419a174d67986eb487740089f6e81e9697
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-03-13 19:13:39 +0100

    Changing the users info - frontend

--- fullstack2-front/src/users/EditUser.js
+++ fullstack2-front/src/users/EditUser.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-import React, {useState} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 export default function EditUser() {
 
     let navigate = useNavigate();
+
+    const {id} = useParams();
 
     const [user, setUser] = useState({
         name:"",
@@ -18,10 +20,19 @@ export default function EditUser() {
         setUser({...user,[e.target.name]:e.target.value})
     }
 
+    useEffect(()=>{
+        loadUser();
+    }, []);
+
     const onSubmit = async (e) => {
         e.preventDefault();
-        await axios.post("http://localhost:8080/user", user);
+        await axios.put(`http://localhost:8080/user/${id}`, user);
         navigate("/");
+    }
+
+    const loadUser = async () => {
+        const result = await axios.get(`http://localhost:8080/user/${id}`);
+        setUser(result.data);
     }
 
   return (
